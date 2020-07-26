cc5313115691299758dba93e7bdb59b0dfbe838b
Ivan <electro460@gmail.com>
2020-07-26 08:47:42 +0300

    Initial commit

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\Ivan\YandexDisk\prpg\py\DRF_Stepik_W01\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
